c348b04ab99dd65978a9a68a91d1717850b1e3b9
Sean Brage <seanmbrage@me.com>
2019-04-30 08:53:47 -0600

    remove backup files

---
+++ init-old.el
@@ -0,0 +1,47 @@
+;; init.el --- emacs configuration by Sean
+;; Commentary:
+;; Custom configuration, some copied from Andrew Jarrett's excellent config
+;; Author Information
+;;; Code:
+(setq inhibit-startup-message t)
+
+;; configure package management and installation
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/")
+             '("org" . "http://orgmode.org/elpa/"))
+
+(package-initialize)
+
+;; use use-package because it's what people use
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(setq use-package-always-ensure t)
+(require 'use-package)
+
+;; set PATH and EXEC-PATH stuff
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+;; read configuration from org file
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+
+;;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("28bf1b0a72e3a1e08242d776c5befc44ba67a36ced0e55df27cfc7ae6be6c24d" default)))
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
